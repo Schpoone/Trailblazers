26b3f4579174e14accf7ee4b67b50325c2ff617e
1j4k7 <jasonkuo147@gmail.com>
2015-03-16 16:08:38 -0400

    I HATE YOU

--- src/Robot.java
+++ src/Robot.java
@@ -7,6 +7,7 @@ import lejos.hardware.sensor.EV3GyroSensor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.RegulatedMotor;
 import lejos.robotics.pathfinding.Path;
+
 /**
  * This represents the physical robot with all of its systems.
  * @author Jason
