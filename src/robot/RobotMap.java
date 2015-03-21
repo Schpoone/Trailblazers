dc8e2bdac441fd1de172a42d945980f6db269f43
1j4k7 <jasonkuo147@gmail.com>
2015-03-20 23:22:46 -0400

    some loose ends and finding out that WADAFUQ IS GOING ON

--- src/robot/RobotMap.java
+++ src/robot/RobotMap.java
@@ -62,17 +62,17 @@ public class RobotMap {
 	/**
 	 * the left motor port
 	 */
-	public final char LEFT_MOTOR;
+	public final char LEFT_MOTOR = 'A';
 	
 	/**
 	 * the right motor port
 	 */
-	public final char RIGHT_MOTOR;
+	public final char RIGHT_MOTOR = 'D';
 	
 	/**
 	 * the motor port for the motor that controls the ultrasonic sensor
 	 */
-	public final char ULTRASONIC_MOTOR;
+	public final char ULTRASONIC_MOTOR = 'B';
 	
 	
 	//================================ Sensors ================================
@@ -80,17 +80,17 @@ public class RobotMap {
 	/**
 	 * port for the left color sensor
 	 */
-	public final Port LEFT_COLOR;
+	public final Port LEFT_COLOR = SensorPort.S1;
 	
 	/**
 	 * port for the right color sensor
 	 */
-	public final Port RIGHT_COLOR;
+	public final Port RIGHT_COLOR = SensorPort.S4;
 	
 	/**
 	 * port for the ultrasonic sensor
 	 */
-	public final Port ULTRASONIC;
+	public final Port ULTRASONIC = SensorPort.S3;
 	
 	
 	
@@ -117,28 +117,8 @@ public class RobotMap {
 		/*60-64*/{},{},{},{},{},
 		/*65-69*/{},{},{},{},{},
 		};	
+		this.PATH_PROPERTIES = new int[1][1];//I HAVE NO IDEA WHAT THIS DOES
 		
-		this.DIRECTION = 0;
-		this.TYPE = 1;
-		this.SPEED = 2;
-		int c = 0;
-		this.FORWARD = c++;
-		this.LEFT = c++;
-		this.RIGHT = c++;
-		this.PARKING = c++;
-		this.INTERSECTION = c++;
-		this.FAST = 740;
-		this.SLOW = 500;
-		
-		this.INTERSECTION_DISTANCE = 100; // 100 is temporary, pls fix
-		
-		this.LEFT_MOTOR = 'A';
-		this.RIGHT_MOTOR = 'D';
-		this.ULTRASONIC_MOTOR = 'B';
-		
-		this.LEFT_COLOR = SensorPort.S1;
-		this.RIGHT_COLOR = SensorPort.S4;
-		this.ULTRASONIC = SensorPort.S3;
 		System.out.println("RobotMap init end");
 	}
 	
@@ -147,27 +127,27 @@ public class RobotMap {
 		return null;
 	}
 	
-	public initMap{
+//	public void initMap() {
 		//{properties, nextnode, direction, length (cm), no. of stop signs, speed (cm/sec)}
-		int n = 0, ne= 45, e = 90, se = 135, s = 180, sw = -135, w = -90, nw = -45;
-		int fast = 16, int slow = 10;
+//		int n = 0, ne= 45, e = 90, se = 135, s = 180, sw = -135, w = -90, nw = -45;
+//		int fast = 16; int slow = 10;
 		
-		this.PATH_PROPERTIES = new int[][]{ // mebbe read from file
-		/*0-4*/  {0,11,n,25,1,fast},{0,12,s,25,1,fast},{1,32,e,560,3,fast},{1,24,},{},
-		/*5-9*/  {},{},{},{},{},
-		/*10-14*/{},{},{},{},{},
-		/*15-19*/{},{},{},{},{},
-		/*20-24*/{},{},{},{},{},
-		/*25-29*/{},{},{},{},{},
-		/*30-34*/{},{},{},{},{},
-		/*35-39*/{},{},{},{},{},
-		/*40-44*/{},{},{},{},{},
-		/*45-49*/{},{},{},{},{},
-		/*50-54*/{},{},{},{},{},
-		/*55-59*/{},{},{},{},{},
-		/*60-64*/{},{},{},{},{},
-		/*65-69*/{},{},{},{},{},
-		};
-		return null;
-	}
+//		this.PATH_PROPERTIES = new int[][]{ // mebbe read from file
+//		/*0-4*/  {0,11,n,25,1,fast},{0,12,s,25,1,fast},{1,32,e,560,3,fast},{1,24,},{},
+//		/*5-9*/  {},{},{},{},{},
+//		/*10-14*/{},{},{},{},{},
+//		/*15-19*/{},{},{},{},{},
+//		/*20-24*/{},{},{},{},{},
+//		/*25-29*/{},{},{},{},{},
+//		/*30-34*/{},{},{},{},{},
+//		/*35-39*/{},{},{},{},{},
+//		/*40-44*/{},{},{},{},{},
+//		/*45-49*/{},{},{},{},{},
+//		/*50-54*/{},{},{},{},{},
+//		/*55-59*/{},{},{},{},{},
+//		/*60-64*/{},{},{},{},{},
+//		/*65-69*/{},{},{},{},{},
+//		};
+		//return null;
+//	}
 }
